f8bfafb9fb1a8450e9759f8c52a6f9c97cf0f5d1
R Coyotl <coyotl101093@gmail.com>
2017-03-24 14:13:59 -0400

    Add files via upload

--- SIngleMatrixNetwork.cpp
+++ SIngleMatrixNetwork.cpp
@@ -11,21 +11,6 @@ const int maxCycles = 50;
 
 int main(int argc, char* argv[])
 {
-	/*
-		Network xor(2, 2, 1, "xor.txt"); // creates your desired network and prints it to a file
-		Network xor("xor.txt"); // uses your created network
-
-		double Xor_input[2];
-
-		Xor_input[0] = Xor_input[1] = 0.0;
-		xor.setNetworkInput(Xor_input);
-
-		xor.cycleNetwork();
-		xor.printNetworkOutputState();
-		xor.writeNetworkOutputStateToFile("xor_out.txt");
-	*/
-
-
 	//Network wOscillator(1,2,2,"oscillator.txt");
 	Network wOscillator("oscillator.txt");
 	Network wOscillator2("oscillator2.txt");
@@ -36,17 +21,17 @@ int main(int argc, char* argv[])
 	while (i < maxCycles) {
 
 		currentCycle += i;
-	//	if (currentCycle % 2 == 0) {
+		if (currentCycle % 2 == 0) {
 
 			Oscillator_input[0] = 1.0;
 			wOscillator.setNetworkInput(Oscillator_input);
 			wOscillator2.setNetworkInput(Oscillator_input);
-	//	}
-		/*else {
-			Oscillator_input[0] = 0.0;
+		}
+		else {
+			Oscillator_input[0] = 1.0;
 			wOscillator.setNetworkInput(Oscillator_input);
 			wOscillator2.setNetworkInput(Oscillator_input);
-		}*/
+		}
 
 		//squashingFunction(1, 1, -2, 0); 
 		/*	
@@ -58,11 +43,11 @@ int main(int argc, char* argv[])
 		wOscillator.cycleNetwork();
 		wOscillator.printNetworkOutputState();
 		//std::cout << "Current Cycle: " << currentCycle << std::endl;
-		wOscillator.writeNetworkOutputStateToFile("Oscillator_out3.txt", currentCycle);
+		wOscillator.writeNetworkOutputStateToFile("NEWOscillator_out_9_cont1.txt", currentCycle);
 
 		wOscillator2.cycleNetwork();
 		//wOscillator2.printNetworkOutputState();
-		wOscillator2.writeNetworkOutputStateToFile("Oscillator_out4.txt", currentCycle);
+		wOscillator2.writeNetworkOutputStateToFile("NEWOscillator_out10_cont1.txt", currentCycle);
 
 		i++;
 	}
